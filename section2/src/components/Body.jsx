6a0ae6d48069aaf8fe2bd4eef2f4501f357750a6
JiWoongE <rlawldnd515@kyonggi.ac.kr>
2024-03-25 21:06:35 +0900

    init project

---
+++ section2/src/components/Body.jsx
@@ -0,0 +1,41 @@
+import './Body.css';
+import {useState} from "react";
+
+function Lightbulb({light}) {
+    return (
+    <>
+        {light === "ON" ? (
+        <div style={{backgroundColor:"orange"}}>ON</div>
+        ) : (
+        <div style ={{backgroundColor:"gray"}}>OFF</div>
+        )}
+    </>
+    )
+}
+
+function StaticLightbulb () {
+    console.log("dd");
+    return (
+        <div style ={{backgroundColor:"gray"}}>OFF</div>
+    )
+}
+
+export default function Body() {
+    const [light, setLight] = useState("OFF");
+    
+    return <div className="body">
+    <Lightbulb light={light}/>
+    <StaticLightbulb />
+    <button onClick={()=> {
+        setLight("ON");
+    }}
+    >켜기
+    </button>
+    <button onClick={()=> {
+        setLight("OFF");
+    }}
+    >끄기
+    </button>
+    </div>
+    
+}
