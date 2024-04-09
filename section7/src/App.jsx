e6117a13a594c6049eebab667f169f277472a896
JiWoongE <147473223+JiWoongE@users.noreply.github.com>
2024-04-09 15:19:40 +0900

    Create - 새로운 투두 생성

--- section7/src/App.jsx
+++ section7/src/App.jsx
@@ -1,16 +1,52 @@
+import { useRef, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import TodoEditor from './components/TodoEditor';
 import TodoList from './components/TodoList';
 
+const mockData = [
+  {
+    id: 0,
+    isDone: true,
+    content: 'React 공부하기',
+    createDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래 널기',
+    createDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: true,
+    content: '음악 연습하기',
+    createDate: new Date().getTime(),
+  },
+];
+
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <TodoEditor />
-            <TodoList />
-        </div>
-    );
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content,
+      createDate: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <TodoEditor onCreate={onCreate} />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
