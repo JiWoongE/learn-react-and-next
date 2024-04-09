6da02e30ebe74794c9dd126e305fa98cc6a4647a
JiWoongE <147473223+JiWoongE@users.noreply.github.com>
2024-04-09 21:21:17 +0900

    Update - 투두 리스트 수정

--- section7/src/components/TodoItem.jsx
+++ section7/src/components/TodoItem.jsx
@@ -1,9 +1,13 @@
 import './TodoItem.css';
 
-export default function TodoItem({ id, isDone, createDate, content }) {
+export default function TodoItem({ id, isDone, createDate, content, onUpdate }) {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
   return (
     <div className="TodoItem">
-      <input type="checkbox" checked={isDone} />
+      <input onChange={onChangeCheckbox} type="checkbox" checked={isDone} />
       <div className="content">{content}</div>
       <div className="date">{new Date(createDate).toLocaleDateString()}</div>
       <button>삭제</button>
